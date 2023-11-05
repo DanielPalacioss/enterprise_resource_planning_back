1215f017cd2d1ce19fd93d7458ccf027ddeea996
Daniel <arindaniel17@gmail.com>
2023-11-05 12:35:31 -0500

    Microservices are saved.

---
+++ orders-service/src/test/java/com/salesmanagementplatform/orders/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesmanagementplatform.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
