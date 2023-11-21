1dc52a87bb29d21796c080a9a680bd24b8608ca9
Daniel <arindaniel17@gmail.com>
2023-11-21 09:51:00 -0500

    Ingreso del servicio invoces

---
+++ invoces-service/src/main/java/com/salesmanagementplatform/invoces/InvocesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.salesmanagementplatform.invoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvocesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvocesServiceApplication.class, args);
+	}
+
+}
---
+++ invoces-service/src/test/java/com/salesmanagementplatform/invoces/InvocesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesmanagementplatform.invoces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvocesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
