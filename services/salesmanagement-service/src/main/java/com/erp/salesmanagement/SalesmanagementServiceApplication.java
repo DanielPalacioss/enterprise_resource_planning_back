48d797d30525ea89022f7d465161e936f61e6167
Daniel <arindaniel17@gmail.com>
2024-02-19 22:09:09 -0500

    solucion

--- services/salesmanagement-service/src/main/java/com/erp/salesmanagement/SalesmanagementServiceApplication.java
+++ services/salesmanagement-service/src/main/java/com/erp/salesmanagement/SalesmanagementServiceApplication.java
@@ -2,7 +2,9 @@ package com.erp.salesmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SalesmanagementServiceApplication {
 
